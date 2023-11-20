74e96f80821dc96518f6631e43ae3d2c85ab49e6
Vincent <vpl5697@gmail.com>
2023-11-20 01:24:24 -0800

    menu works with animation
    x button doesnt work though, menu overlay is blocking (find an issue later on)

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navbar = document.querySelector('.navbar');
+const menu_btn = document.querySelector('.hamburger');
+const hamburg_menu = document.querySelector('.in-menu');
+const menuItems = document.querySelectorAll('.in-menu a');
 
-menuToggle.addEventListener('click', () => {
-    navbar.classList.toggle('active');
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    hamburg_menu.classList.toggle('is-active');
 });
 
-window.addEventListener('resize', () => {
-    if (window.innerWidth > 768) {
-        navbar.classList.remove('active');
-    }
+// Close the menu when a menu item is clicked
+menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', () => {
+        hamburg_menu.classList.remove('is-active');
+        menu_btn.classList.remove('is-active');
+        document.body.classList.remove('menu-open'); // Add this line
+    });
 });
